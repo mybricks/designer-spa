f1e246174525de1094c877b10a56408df8b38526
GodStream <zpqwangyiyouxiang@163.com>
2022-11-22 20:57:54 +0800

    feat: preview support micro app

--- src/preview.tsx
+++ src/preview.tsx
@@ -1,4 +1,4 @@
-import {render} from 'react-dom'
+import { render, unmountComponentAtNode } from 'react-dom'
 import {render as renderUI} from '@mybricks/render-web'//使用Mybricks-web渲染器
 
 //连接器运行时
@@ -20,9 +20,11 @@ try {
 
 //----------------------------------------------------------------------------
 
-render(<Page/>, document.querySelector('#root'))
+function renderMethod (props) {
+  render(<Page mainProps={props} />, props.container ? props.container.querySelector('#root') : document.getElementById('root'));
+}
 
-function Page() {
+function Page({ mainProps }) {
   return (
     <div>
       {
@@ -31,10 +33,41 @@ function Page() {
             i18n(text) {//多语言
               return text
             },
-            callConnector: call
+            callConnector: call,
+            getMainProps: () => { // 获取主应用参数方法，如：token等参数，取决于主应用传入
+              return mainProps
+            }
           },
         })
       }
     </div>
   )
+}
+
+/**
+ * bootstrap 只会在微应用初始化的时候调用一次，下次微应用重新进入时会直接调用 mount 钩子，不会再重复触发 bootstrap。
+ * 通常我们可以在这里做一些全局变量的初始化，比如不会在 unmount 阶段被销毁的应用级别的缓存等。
+ */
+ export async function bootstrap() {
+  console.log('react app bootstraped');
+}
+
+/**
+ * 应用每次进入都会调用 mount 方法，通常我们在这里触发应用的渲染方法
+ */
+export async function mount(props) {
+  renderMethod(props)
+}
+
+/**
+ * 应用每次 切出/卸载 会调用的方法，通常在这里我们会卸载微应用的应用实例
+ */
+export async function unmount(props) {
+  unmountComponentAtNode(
+    props.container ? props.container.querySelector('#root') : document.getElementById('root'),
+  );
+}
+
+if (!window.__POWERED_BY_QIANKUN__) { // 非微前端嵌入环境，走默认渲染方式
+  renderMethod({})
 }
